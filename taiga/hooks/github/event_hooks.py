2004b7798fb0f42deb997a865ec103f50bff271f
alvarollmenezes <alvarollmenezes@gmail.com>
2016-06-28 13:32:38 -0300

    Refatora o hook do github

--- taiga/hooks/github/event_hooks.py
+++ taiga/hooks/github/event_hooks.py
@@ -122,9 +122,6 @@ def replace_github_references(project_url, wiki_text):
 
 class IssuesEventHook(BaseEventHook):
     def process_event(self):
-        if self.payload.get('action', None) != "opened":
-            return
-
         number = self.payload.get('issue', {}).get('number', None)
         subject = self.payload.get('issue', {}).get('title', None)
         github_url = self.payload.get('issue', {}).get('html_url', None)
@@ -140,6 +137,20 @@ class IssuesEventHook(BaseEventHook):
         if not all([subject, github_url, project_url]):
             raise ActionSyntaxException(_("Invalid issue information"))
 
+        if self.payload.get('action', None) == "opened":
+            _process_opened(self, number, subject, github_url, user, github_user_name, github_user_url, project_url, description)
+        if self.payload.get('action', None) == "edited":
+            _process_edited(number, subject, github_url, user, github_user_name, github_user_url, project_url, description)
+
+
+    def _process_edited(number, subject, github_url, github_user_id, github_user_name, github_user_url, project_url, description):
+        issues = Issue.objects.filter(external_reference=["github", github_url])
+
+        for item in list(issues):
+            return
+
+
+    def _process_opened(self, number, subject, github_url, github_user_id, github_user_name, github_user_url, project_url, description):
         issue = Issue.objects.create(
             project=self.project,
             subject=subject,
